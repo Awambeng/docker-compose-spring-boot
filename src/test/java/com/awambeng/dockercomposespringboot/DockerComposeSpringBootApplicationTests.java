090329c06ddcfd5b12befdfa41f2ea16127a5454
Awambeng <awambengrodrick@gmail.com>
2024-03-15 09:44:04 +0100

    add project setup

---
+++ src/test/java/com/awambeng/dockercomposespringboot/DockerComposeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awambeng.dockercomposespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
