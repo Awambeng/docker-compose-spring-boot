654ab2744fc9098bce08d3193feb278024aae9a3
Awambeng <awambengrodrick@gmail.com>
2024-03-15 11:17:49 +0100

    add user repository

---
+++ src/main/java/com/awambeng/dockercomposespringboot/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.awambeng.dockercomposespringboot.repository;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.awambeng.dockercomposespringboot.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    
+}
